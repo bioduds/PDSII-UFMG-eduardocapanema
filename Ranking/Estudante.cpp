c3e291afd081f128b7dcdbeaf8d45663108a108a
bioduds <bioduds@gmail.com>
2020-08-10 15:46:38 +0000

    Moved Back Ranking

---
+++ Ranking/Estudante.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+float Estudante::calcularRSG() {
+
+} 
+
+    // Atributos
+    //std::string nome;
+    //int matricula;
+    //float notas[5];
+
---
+++ Ranking/Estudante.hpp
@@ -0,0 +1,17 @@
+#ifndef ESTUDANTE_H
+#define ESTUDANTE_H
+#include <string>
+
+struct Estudante {
+
+    // Atributos
+    std::string nome;
+    int matricula;
+    float notas[5];
+
+    // Métodos
+    float calcularRSG();
+
+};
+
+#endif 
