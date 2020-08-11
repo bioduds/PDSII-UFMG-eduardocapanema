2bf8d5385f23b957c28e6c6d670a8e1255fbb988
bioduds <bioduds@gmail.com>
2020-08-11 20:52:24 +0000

    Testar Ponteiros

--- ponteiros.cpp
+++ ponteiros.cpp
@@ -17,13 +17,14 @@ int main() {
     std::cout << *long_ptr << std::endl;
     
     //Imprima o valor de value2
-    long_ptr = &value2;
+    value2 = *long_ptr;
+    std::cout << value2 << std::endl;
     
     //Imprima o endereço de value1
     std::cout << &value1 << std::endl;
     
     //Imprima o endereço armazenado em long_ptr
-    std::cout << &long_ptr << std::endl;
+    std::cout << long_ptr << std::endl;
     
     //Imprima a comparação entre o endereço de value1 com o endereço armazenado em long_ptr
     std::cout << (&value1 == long_ptr) << std::endl;
@@ -38,13 +39,13 @@ int main() {
     //int vS = *(&values + 1) - values;
     //std::cout << "size vS: " << vS << std::endl;
     for( int i=0; i<5; i++ ) {
-        std::cout << values[i] << std::endl;
+        std::cout << *(values + i) << std::endl;
     }
 
     //imprimir os elementos do array values usando notação de array ponteiro/deslocamento com o nome de array como o ponteiro.
-    //std::cout << "Ponteiro Deslocamento " << vS << std::endl;
+    //std::cout << "Ponteiro Deslocamento vptr" << std::endl;
     for( int i=0; i<5; i++ ) {
-        std::cout << *(values + i) << std::endl;
+        std::cout << *(v_ptr + i) << std::endl;
     }
     
     //imprimir os elementos do array values utilizando subscritos no ponteiro para o array.
@@ -54,16 +55,19 @@ int main() {
     }
     
     //imprimir o quinto elemento de values utilizando a notação de subscrito de array,
-    std::cout << "Quinto elemento " << values[4] << std::endl;
+    std::cout << values[4] << std::endl;
     
     //a notação de ponteiro/deslocamento com o nome de array como o ponteiro, 
+    std::cout << *(values + 4) << std::endl;
     
     //a notação de subscrito de ponteiro,
+    std::cout << v_ptr[4] << std::endl;
     
     //a notação de ponteiro/deslocamento.
+    std::cout << *(v_ptr + 4) << std::endl;
     
     //imprimir a comparação entre o valor armazenado em (v_ptr-4) e values[0]
-    std::cout << "Comparacao: " << ( *(v_ptr-4) == values[0]) << std::endl;
+    std::cout << ( *(v_ptr-4) == values[0]) << std::endl;
 
     return 0;
 }
